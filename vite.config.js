7e459e4971139ac3ad6ff66c1fddb0603b85a6fc
Karthika-ara <ara.karthika.n@gmail.com>
2026-07-16 17:01:54 +0530

    1607

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/Kayo-Schl-Chennai/" : "/",
+  base: command === "build" ? "/saravanampatti/" : "/",
   plugins: [react()],
 }));
